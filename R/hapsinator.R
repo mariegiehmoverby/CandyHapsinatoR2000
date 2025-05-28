0539d92823cf6f75f7f173c00574def083e24bd0
Marie Giehm <Your Email>
2025-05-28 14:15:15 +0200

    Setup of the package

---
+++ R/hapsinator.R
@@ -0,0 +1,16 @@
+
+
+#' hapsinator
+#' @param pricePer100g price of 100g of candy
+#' @param snollerMoney money to spend on candy 
+#' @param numRows number of rows in the candy shelf
+#' @param numCols number of columns in the candy shelf
+#' @param handfulSize handful size (small, medium or large)
+#' 
+#' @export 
+#' @return print(pricePer100g) 
+#' @examples bla
+
+hapsinator <- function(pricePer100g, snollerMoney, numRows, numCols, handfulSize) {
+    print(pricePer100g)
+}
