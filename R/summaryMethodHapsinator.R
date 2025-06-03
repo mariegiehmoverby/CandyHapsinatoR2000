5c5e8c1502213003d9eeec295aea97fefe9cc6e2
Marie Giehm <Your Email>
2025-06-03 11:40:15 +0200

    update

--- R/summaryMethodHapsinator.R
+++ R/summaryMethodHapsinator.R
@@ -1,24 +1,23 @@
 #' @title Summary method for hapsinatoR objects
 #' @description Prints a concise summary of a \code{hapsinatoR} object, including the number of selected candies (haps),
-#' total weight to mix, number of unique tiles, and their coordinates.
-#' @param object An object of class \code{hapsinatoR}, typically created by the \code{hapsinatoR()} function.
-#' @param ... Additional arguments (currently unused).
-#' @return Returns a summary on the output from the hapsinatoR object
+#' total weight to mix, number of unique candies, and coordinates of the candies to grab.
+#' @param x An object of class \code{hapsinatoR}, typically created by the \code{hapsinatoR()} function.
+#' @return Returns a summary on the hapsinatoR object
 #' @export
 #' @method summary hapsinatoR 
-summary.hapsinatoR <- function(object, ...) {
+summary.hapsinatoR <- function(x) {
   cat("=================================\n")
   cat("Summary of hapsinatoR object\n")
   cat("=================================\n")
   
-  cat("Number of different candies:", object$uniqueCandies, "\n")
-  cat("Number of candies to GRAB:", object$numHaps, "\n")
-  cat("Total grams to mix:", round(object$gramsToMix), "\n")
-  cat("Total grams mixed:", round(sum(object$hapsSizes)), "\n")
+  cat("Number of different candies:", x$uniqueCandies, "\n")
+  cat("Number of candies to GRAB:", x$numHaps, "\n")
+  cat("Total grams to mix:", round(x$gramsToMix), "\n")
+  cat("Total grams mixed:", round(sum(x$hapsSizes)), "\n")
   
   cat("=================================\n")
   cat("Candies to GRAB are at locations:\n")
-  grab_tiles <- subset(object$candyShelf, status == "GRAB")
+  grab_tiles <- subset(x$candyShelf, status == "GRAB")
   
   if (nrow(grab_tiles) == 0) {
     cat("None\n")
@@ -28,4 +27,3 @@ summary.hapsinatoR <- function(object, ...) {
     cat("\n")
   }
 }
-
